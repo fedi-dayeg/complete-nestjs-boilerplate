2b7f284ab88d3c7438378b94d6a63d8f932aec02
Fedi dayeg <fedidayeg@MacBook-Pro-de-Fedi.local>
2025-11-16 12:51:34 +0100

    feat(common): refactored module for version 2

--- src/common/error/serializations/error.serialization.ts
+++
@@ -1,3 +0,0 @@
-import { ResponseMetadataSerialization } from 'src/common/response/serializations/response.default.serialization';
-
-export class ErrorMetadataSerialization extends ResponseMetadataSerialization {}
--- src/common/setting/decorators/setting.admin.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { SettingNotFoundGuard } from 'src/common/setting/guards/setting.not-found.guard';
-import { SettingPutToRequestGuard } from 'src/common/setting/guards/setting.put-to-request.guard';
-
-export function SettingUpdateGuard(): MethodDecorator {
-    return applyDecorators(
-        UseGuards(SettingPutToRequestGuard, SettingNotFoundGuard)
-    );
-}
