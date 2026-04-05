2a56eae357af3a91b401bcefc0728532f36d7b00
fedidayeg93 <fedi.dayeg@pit.fr>
2026-04-05 11:56:22 +0200

    fix(*): bugs fixed and improvement

--- src/modules/user/controllers/user.user.controller.ts
+++ src/modules/user/controllers/user.user.controller.ts
@@ -17,7 +17,7 @@ import { UserUserDeleteSelfDoc } from '@modules/user/docs/user.user.doc';
 import { UserService } from '@modules/user/services/user.service';
 import { Controller, Delete } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { EnumRoleType, GeoLocation, UserAgent } from '@prisma/client';
+import { EnumRoleType, GeoLocation, UserAgent } from '@generated/prisma-client';
 
 @ApiTags('modules.user.user')
 @Controller({
